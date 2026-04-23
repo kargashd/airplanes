d7bbf5887f9dfd907365b3b1ccfab1a7cb72a72b
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2026-04-23 09:14:09 +0300

    add abstract classes

---
+++ src/abstract_file.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+
+
+class AbstractFile(ABC):
+    """Абстрактный класс для работы с файлами"""
+
+    @abstractmethod
+    def add_eroplane(self, aeroplane: Dict[str, Any]) -> None:
+        """Добавляет информацию о самолёте в файл"""
+        pass
+
+    @abstractmethod
+    def get_aeroplanes(self, **filters) -> List[Dict[str, Any]]:
+        """Получает данные из файла по указанным критериям"""
+        pass
+
+    @abstractmethod
+    def delete_aeroplane(selfself, **criteria) -> None:
+        """Удаляет информацию о самолётах по указанным критериям"""
+        pass
